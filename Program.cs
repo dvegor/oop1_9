c6567264e92496fd2759a21a3825d774712e89d4
d_ego <dvegor@gmail.com>
2024-02-11 21:51:35 +0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+
+namespace ConsoleApp3
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "{\"Name\":\"Semen\",\"Surename\":\"Semenov\",\"ID\":1,\"DEPT\":\"Teacher\"}";
+
+            var jsonObj = JsonConvert.DeserializeObject(json) as JObject;
+
+            XDocument xmlDoc = new XDocument(new XElement("Root", jsonObj.Properties().Select(p => new XElement(p.Name, p.Value))));
+            xmlDoc.Save("output.xml");
+        }
+
+
+    }
+}
